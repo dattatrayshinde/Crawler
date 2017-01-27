b2b29169564db3acbedeaf9a8cfbc5803ff4d6f6
DattatrayShinde <dattatrayshindeharipur@gmail.com>
2017-01-28 01:44:09 +0530

    Code to crawal a single page
    Code to crawal a single page

--- items.py
+++ items.py
@@ -6,9 +6,14 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy.item import Item, Field
 
 class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    #Item will include follwing details -
+    #Article text, author, headline, article url, etc
+    text = Field()
+    author = Field()
+    headline = Field()
+    url = Field()
--- spiders/bbc.py
+++ spiders/bbc.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy import Spider
+from scrapy.selector import Selector
+from Crawler.items import CrawlerItem
 
 class BbcSpider(scrapy.Spider):
     name = "bbc"
+    #Crawler will no go beyond this domains
     allowed_domains = ["bbc.com"]
+    #Where to start crawling
     start_urls = (
-        'http://www.bbc.com/',
+        'http://www.bbc.com/news/world-us-canada-38777437',
     )
-
+    
+    
     def parse(self, response):
-        pass
+        #fetching the title of the news
+        item = CrawlerItem()
+        item['text'] = response.xpath('//*[@id="page"]/div[2]/div[2]/div/div[1]/div[1]/div[3]/p[1]//text()').extract()[0]
+        item['author'] = "BBC News Media"
+        item['headline'] = response.xpath('//*[@id="page"]/div[2]/div[2]/div/div[1]/div[1]/h1//text()').extract()[0]
+        item['url'] = response.url
+        yield item      
+        
